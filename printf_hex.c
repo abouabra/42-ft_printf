55dbf07cf549ac74bdf7e6b5aba39d12ceaa0097
Ayman S15 <aymanbouabra@gmail.com>
2022-11-03 18:56:34 +0100

    1337

--- printf_hex.c
+++ printf_hex.c
@@ -35,33 +35,97 @@ void	put_hex_original(unsigned int nb, char x,t_vars *vars)
 
 void	ft_put_hex_nbr(unsigned int nb, char x,t_vars *vars)
 {
+	int len_of_int;
+	len_of_int = hex_len(nb);
+	if(nb==0 && vars->flags[precision] == 1)
+		len_of_int--;
 	handle_width(vars,hex_len(nb));
-
-	if(vars->flags[zero] == 1)
-    {
-        handle_padding(vars,vars->flag_counter[zero],hex_len(nb),'0');
+	if(vars->state == 0)
+	{
         put_hex_original(nb,x,vars);
-        vars->flags[zero] = 0;
-    }
-	else if(vars->flags[minus] == 1)
+		return;
+	}
+	if(vars->flags[zero] == 1)
     {
 		if(vars->flags[precision] == 1)
 		{
-			handle_padding(vars,vars->flag_counter[precision],hex_len(nb),'0');
-			put_hex_original(nb,x,vars);
-			handle_padding(vars,vars->flag_counter[minus],vars->flag_counter[precision],' ');
+			if(vars->flag_counter[precision] < hex_len(nb))
+				vars->flag_counter[precision] = len_of_int;
+			
+			//printf("len_of_int %d %d\n",vars->flag_counter[zero],vars->flag_counter[precision]-1);
+			handle_padding(vars,vars->flag_counter[zero],vars->flag_counter[precision],' ');
+			// if(n < 0)
+			// {
+			// 	ft_putchar('-', vars);
+			// 	vars->flag_counter[precision]--;
+			// 	len_of_int--;
+			// }
+			handle_padding(vars,vars->flag_counter[precision],len_of_int,'0');
+			if(nb != 0)
+        		put_hex_original(nb,x,vars);
+			
+			//printf("======");
+			vars->flags[zero] =0;
+			vars->int_len[zero] = 0;
+			vars->flag_counter[zero] = 0;
+			
+			vars->flags[precision] =0;
+			vars->int_len[precision] = 0;
+			vars->flag_counter[precision] = 0;
+			vars->state=0;
 			return;
 		}
-        put_hex_original(nb,x,vars);
-        handle_padding(vars,vars->flag_counter[minus],hex_len(nb),' ');
-        vars->flags[minus] = 0;
+		else
+		{
+        	handle_padding(vars,vars->flag_counter[zero],len_of_int,'0');
+        	put_hex_original(nb,x,vars);
+		}
+        vars->flags[zero] = 0;
+		vars->int_len[zero] = 0;
+		vars->flag_counter[zero] = 0;
+		vars->state=0;
     }
-	else if(vars->flags[precision] == 1)
+	if(vars->flags[minus] == 1)
     {
-        handle_padding(vars,vars->flag_counter[precision],hex_len(nb),'0');
-		put_hex_original(nb,x,vars);
-        vars->flags[precision] = 0;
+		if(vars->flags[precision] == 1)
+		{
+			//printf("len_of_int %d %d\n",vars->flag_counter[precision],vars->flag_counter[minus]);
+			handle_padding(vars,vars->flag_counter[precision],len_of_int,'0');
+			if(nb != 0)
+	        	put_hex_original(nb,x,vars);
+			// if(n<0)
+			// 	vars->flag_counter[minus]--;
+			if(vars->flag_counter[precision] < hex_len(nb))
+				vars->flag_counter[precision] = len_of_int;
+			handle_padding(vars,vars->flag_counter[minus],vars->flag_counter[precision],' ');
+			//printf("len_of_int %d %d\n",vars->flag_counter[precision],vars->flag_counter[minus]);
+			vars->flags[minus] =0;
+			vars->int_len[minus] = 0;
+			vars->flag_counter[minus] = 0;
+			
+			vars->flags[precision] =0;
+			vars->int_len[precision] = 0;
+			vars->flag_counter[precision] = 0;
+			vars->state=0;
+			return;
+		}
+	    put_hex_original(nb,x,vars);
+        handle_padding(vars,vars->flag_counter[minus],len_of_int,' ');
+        vars->flags[minus] =0;
+		vars->int_len[minus] = 0;
+		vars->flag_counter[minus] = 0;
+		vars->state=0;
     }
-	else if(vars->state == 0)
-		put_hex_original(nb,x,vars);
+	if(vars->flags[precision] == 1)
+    {
+		//printf("flag . counter: %d len:%d\n",vars->flag_counter[precision],vars->int_len[precision]);
+        handle_padding(vars,vars->flag_counter[precision],len_of_int,'0');
+		//printf("flag . counter: %d len:%d\n",vars->flag_counter[precision],vars->int_len[precision]);
+		if(nb != 0)
+	    put_hex_original(nb,x,vars);
+        vars->flags[precision] = 0;
+		vars->int_len[precision] = 0;
+		vars->flag_counter[precision] = 0;
+		vars->state=0;
+    }
 }
